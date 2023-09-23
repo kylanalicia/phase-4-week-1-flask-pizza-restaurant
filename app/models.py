c27d600e5080bc139d902515f5489e7149e80797
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-23 10:42:08 +0300

    created a db for pizza and restaurantpizza

--- app/models.py
+++ app/models.py
@@ -8,3 +8,14 @@ class Restaurant(db.Model):
     address = db.Column(db.String(200), nullable=False)
     restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True)
 
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy=True)
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False, check_constraint="price >= 1 AND price <= 30")
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
