9e3b909ca377c41e170ebefeef0d29286cb7d187
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-23 10:40:25 +0300

    created a db for restaurant

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
-# from flask import Flask
+from sqlalchemy import SQLalchemy
 
-# app = Flask(__name__)
+db = SQLalchemy()
 
-# def create_app():
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True)
 
-#     return app
