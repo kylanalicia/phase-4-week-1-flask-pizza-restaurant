82f8524ba2535a6a397051a7e8e960a0f4f15e35
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-23 12:22:40 +0300

    migrate app and db

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager
 from flask_migrate import Migrate , MigrateCommand
 
 app = create_app()
+
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
