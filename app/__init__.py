b17af366f93c12212e34fd026cddd7ff13eed6ef
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-24 13:24:16 +0300

    made migrate run by itself in manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from sqlalchemy import SQLAlchemy
-
-# create flass application instance
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite///phase-4-week-1-flask-pizza-restaurant.db'
+from flask_sqlalchemy import SQLAlchemy  # Correct import statement
 
 db = SQLAlchemy()
+
+# Create Flask application instance
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///phase-4-week-1-flask-pizza-restaurant.db'
 
 def create_app():
     from app import routes  # Import routes here
@@ -14,5 +14,3 @@ def create_app():
 
 # Create the app instance using create_app
 app = create_app()
-
-
--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
+# manage.py
 from app import create_app, db
-from flask_script import Manager
-from flask_migrate import Migrate , MigrateCommand
+from flask_migrate import Migrate
 
 app = create_app()
-
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
